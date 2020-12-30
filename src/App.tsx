c912accb56aa390fbb5f707511c01553aa5a6d04
StanislavKaisin <8050361@gmail.com>
2020-12-30 21:24:27 +0200

    button example

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import {
   Button,
   createStyles,
   Grid,
+  IconButton,
   makeStyles,
   Theme,
 } from "@material-ui/core";
 import { Header } from "./components/Header";
 import { Content } from "./components/Content";
 import { CoolBtn } from "./components/CoolBtn";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+import AlarmIcon from "@material-ui/icons/Alarm";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -32,6 +37,35 @@ function App() {
   return (
     <div>
       {/* <Grid container direction="column"> */}
+      <Button
+        color="primary"
+        size="small"
+        disableFocusRipple
+        disableRipple
+        classes={{ root: classes.btnStyle }}
+      >
+        simple button
+      </Button>
+      <Button color="primary" size="medium" variant="outlined">
+        simple button
+      </Button>
+      <Button color="primary" size="large" variant="contained">
+        simple button
+      </Button>
+      <DeleteIcon />
+
+      <IconButton aria-label="delete" disabled color="primary">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+      <IconButton color="primary" aria-label="add to shopping cart">
+        <AddShoppingCartIcon />
+      </IconButton>
+      <hr />
+      <hr />
+
       <Button className={classes.btnStyle}>small button</Button>
       <Button className={classes.btnStyle}>another small button</Button>
       <h1 className={classes.textStyle}>my header</h1>
